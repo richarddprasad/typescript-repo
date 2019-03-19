09fa6f0de7dfa1e349408369399d9b6eae37fd79
Richard Prasad <richarddprasad@gmail.com>
2019-03-19 13:37:20 -0700

    Working on a singly-linked list... need to implement delete.

--- src/datastructures/linked_lists/sllist.ts
+++ src/datastructures/linked_lists/sllist.ts
@@ -2,8 +2,8 @@
 import Node from '../../common/node';
 
 class SLList<T> {
-    private head: Node<T> | null;
-    private tail: Node<T> | null;
+    private head: Node<T>;
+    private tail: Node<T>;
 
     constructor() {
         this.head = new Node<T>();
@@ -13,18 +13,91 @@ class SLList<T> {
     }
 
     public isEmpty(): boolean {
-        let empty = false;
-        if (this.head != null && this.head.next === this.tail) {
-            empty = true;
-        }
-        return empty;
+        // let empty = false;
+        // if (this.head != null && this.head.next === this.tail) {
+        //     empty = true;
+        // }
+        // return empty;
+        return this.head.next === this.tail;
     }
 
-    // TODO: insert()
+    public insert(item: T): void {
+        const newNode: Node<T> = new Node<T>(item);
 
-    // TODO: find()
+        if (this.isEmpty()) {
+            newNode.next = this.tail;
+            this.head.next = newNode;
+        } else {
+            let lastNode = this.advanceTo();
+            newNode.next = this.tail;
+
+            // It's hard to avoid convoluted code in TypeScript... :(
+            if (lastNode) {
+                lastNode.next = newNode;
+            }
+        }
+    } // end fn insert
+
+    // This method tells us if the item is in the list
+    public contains(item: T): boolean {
+        let rv: boolean = false;
+        let searchNode: Node<T> = new Node<T>(item);
+
+        if(!this.isEmpty()) {
+            let current: Node<T> | null = this.head.next;
+            while (current != null && current !== this.tail) {
+                if(current.item === searchNode.item) {
+                    rv = true;
+                    break;
+                }
+                current = current.next;        
+            } // end while
+        } // end if
+
+        return rv;
+    } // end fn contains
+    
+
+    // This method returns the node the sought-after item is in, if it exists
+    public find(item: T): Node<T> | null {
+        let rv: Node<T> | null = null;
+        let searchNode: Node<T> = new Node<T>(item);
+
+        if(!this.isEmpty()) {
+            let current: Node<T> | null = this.head.next;
+            while (current != null && current !== this.tail) {
+                if(current.item === searchNode.item) {
+                    rv = current;
+                    break;
+                }
+                current = current.next;        
+            } // end while
+        } // end if
+
+        return rv;
+    } // end fn find
 
     // TODO: delete()
+
+    // Helper methods
+
+    // This function advances to the node right before the tail node
+    private advanceTo(): Node<T> | null {
+        let current: Node<T> | null = this.head.next;
+        while (current != null && current.next !== this.tail) {
+            current = current.next;
+        }
+        return current;
+    }
+
+    // For debugging
+    public printContents(): void {
+        let current: Node<T> | null = this.head.next;
+        while (current != null && current !== this.tail) {
+            console.log(current.item);
+            current = current.next;
+        }
+    }
 }
 
 export default SLList;
--- src/datastructures/linked_lists/sllist_test.ts
+++ src/datastructures/linked_lists/sllist_test.ts
@@ -4,7 +4,37 @@ const slist: SLList<number> = new SLList<number>();
 
 function listStatus() {
     console.log("isEmpty?: ", slist.isEmpty());
-
+    if (!slist.isEmpty()) {
+        console.log("List contents:");
+        slist.printContents();
+    }
 }
 
 listStatus();
+
+console.log("\nInserting 1st element...");
+slist.insert(1);
+listStatus();
+
+console.log("\nInserting 2nd element...");
+slist.insert(2);
+listStatus();
+
+console.log("\nInserting several elements...")
+for (let i: number = 3; i <= 20; i++) {
+    slist.insert(i);
+}
+listStatus();
+
+
+console.log("Contains 20?: ", slist.contains(20));
+console.log("Contains 21?: ", slist.contains(21));
+
+let test1 = slist.find(20);
+console.log("Node with 20 is null?: ", !test1);
+if(test1) {
+    console.log("Node with 20 contains: ", test1.item);
+}
+let test2 = slist.find(21);
+console.log("Node with 21 is null?: ", !test2);
+
