4fc2cd126b051b5e4d9880eb9edfa605a27ad9aa
Richard Prasad <richarddprasad@gmail.com>
2019-03-19 14:08:54 -0700

    Done with the singly-linked list.

--- src/datastructures/linked_lists/sllist.ts
+++ src/datastructures/linked_lists/sllist.ts
@@ -13,11 +13,6 @@ class SLList<T> {
     }
 
     public isEmpty(): boolean {
-        // let empty = false;
-        // if (this.head != null && this.head.next === this.tail) {
-        //     empty = true;
-        // }
-        // return empty;
         return this.head.next === this.tail;
     }
 
@@ -43,34 +38,34 @@ class SLList<T> {
         let rv: boolean = false;
         let searchNode: Node<T> = new Node<T>(item);
 
-        if(!this.isEmpty()) {
+        if (!this.isEmpty()) {
             let current: Node<T> | null = this.head.next;
             while (current != null && current !== this.tail) {
-                if(current.item === searchNode.item) {
+                if (current.item === searchNode.item) {
                     rv = true;
                     break;
                 }
-                current = current.next;        
+                current = current.next;
             } // end while
         } // end if
 
         return rv;
     } // end fn contains
-    
+
 
     // This method returns the node the sought-after item is in, if it exists
     public find(item: T): Node<T> | null {
         let rv: Node<T> | null = null;
         let searchNode: Node<T> = new Node<T>(item);
 
-        if(!this.isEmpty()) {
+        if (!this.isEmpty()) {
             let current: Node<T> | null = this.head.next;
-            while (current != null && current !== this.tail) {
-                if(current.item === searchNode.item) {
+            while (current && current !== this.tail) {
+                if (current.item === searchNode.item) {
                     rv = current;
                     break;
                 }
-                current = current.next;        
+                current = current.next;
             } // end while
         } // end if
 
@@ -78,13 +73,31 @@ class SLList<T> {
     } // end fn find
 
     // TODO: delete()
+    public delete(item: T): Node<T> | null {
+        let rv: Node<T> | null = null;
+
+        if (this.contains(item)) {
+            // console.log(`${item} was found in the list`);
+            let current: Node<T> = this.head;
+            while (current.next && current.next.item !== item) {
+                current = current.next;
+            }
+            if (current.next) {
+                rv = current.next;
+                current.next = current.next.next;
+            }
+        }
+
+        return rv;
+
+    } // end fn delete
 
     // Helper methods
 
     // This function advances to the node right before the tail node
     private advanceTo(): Node<T> | null {
         let current: Node<T> | null = this.head.next;
-        while (current != null && current.next !== this.tail) {
+        while (current && current.next !== this.tail) {
             current = current.next;
         }
         return current;
@@ -93,7 +106,7 @@ class SLList<T> {
     // For debugging
     public printContents(): void {
         let current: Node<T> | null = this.head.next;
-        while (current != null && current !== this.tail) {
+        while (current && current !== this.tail) {
             console.log(current.item);
             current = current.next;
         }
--- src/datastructures/linked_lists/sllist_test.ts
+++ src/datastructures/linked_lists/sllist_test.ts
@@ -32,9 +32,32 @@ console.log("Contains 21?: ", slist.contains(21));
 
 let test1 = slist.find(20);
 console.log("Node with 20 is null?: ", !test1);
-if(test1) {
+if (test1) {
     console.log("Node with 20 contains: ", test1.item);
 }
 let test2 = slist.find(21);
 console.log("Node with 21 is null?: ", !test2);
 
+console.log("\nDeleting 30 (non-existent)...");
+let test3 = slist.delete(30);
+console.log("Deleting 30 successful?: ", !(test3 == null));
+listStatus();
+
+console.log("\nDeleting 10 (does exist)...");
+let test4 = slist.delete(10);
+console.log("Deleting 10 successful?: ", test4 != null);
+if (test4) {
+    console.log("Value of deleted node (10): ", test4.item);
+}
+listStatus();
+
+console.log("\nDeleting primes...");
+slist.delete(2);
+slist.delete(3);
+slist.delete(5);
+slist.delete(7);
+slist.delete(11);
+slist.delete(13);
+slist.delete(17);
+slist.delete(19);
+listStatus();
