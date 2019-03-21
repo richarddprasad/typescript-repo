c61171cfaac719f0fef1c83edbb017cfbe03cd88
Richard Prasad <richarddprasad@gmail.com>
2019-03-21 13:50:48 -0700

    Implemented insert() for DL List

--- src/datastructures/linked_lists/dllist.ts
+++ src/datastructures/linked_lists/dllist.ts
@@ -5,16 +5,36 @@ class DLList<T> {
     private tail: DNode<T> | null;
 
     constructor() {
-        this.head = null;
-        this.tail = null;
+        this.head = new DNode<T>();
+        this.tail = new DNode<T>();
+
+        this.head.prev = null;
+        this.head.next = this.tail;
+        this.tail.prev = this.head;
+        this.tail.next = null;
     }
 
     public isEmpty(): boolean {
-        return true;
+        return (this.head != null &&
+            this.tail != null &&
+            this.head.next === this.tail &&
+            this.tail.prev === this.head);
     }
 
     public insert(item: T): void {
+        let newNode: DNode<T> = new DNode<T>(item);
+        let current: DNode<T> | null = this.head;
 
+        while (current && current.next !== this.tail) {
+            current = current.next;
+        }
+
+        if (current && current.next) {
+            newNode.next = current.next;
+            newNode.prev = current;
+            current.next.prev = newNode;
+            current.next = newNode;
+        }
     }
 
     public getFirst(): DNode<T> | null {
@@ -43,7 +63,13 @@ class DLList<T> {
 
     // Debugger method
     public printContents(): void {
-
+        if (!this.isEmpty() && this.head) {
+            let current: DNode<T> | null = this.head.next;
+            while (current && current !== this.tail) {
+                console.log(current.item);
+                current = current.next;
+            }
+        }
     }
 }
 
--- src/datastructures/linked_lists/dllist_test.ts
+++ src/datastructures/linked_lists/dllist_test.ts
@@ -2,7 +2,7 @@ import DLList from './dllist';
 
 function listStatus() {
     console.log("isEmpty?: ", dlist.isEmpty());
-    if(!dlist.isEmpty()) {
+    if (!dlist.isEmpty()) {
         console.log("List Contents:");
         dlist.printContents();
     }
@@ -11,4 +11,18 @@ function listStatus() {
 const dlist = new DLList<number>();
 
 console.log("Creating a new doubly-linked list...");
+listStatus();
+
+console.log("\nInserting 1st element...");
+dlist.insert(1);
+listStatus();
+
+console.log("\nInserting 2nd element...");
+dlist.insert(2);
+listStatus();
+
+console.log("\nInserting several elements...");
+for (let i: number = 3; i <= 10; i++) {
+    dlist.insert(i);
+}
 listStatus();
