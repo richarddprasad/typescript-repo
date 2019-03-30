40496e5db2408c78e4e647d692cb08124bfa2841
Richard Prasad <richarddprasad@gmail.com>
2019-03-30 13:36:04 -0700

    Finished max binary heap

--- src/datastructures/heaps/MaxBinaryHeap.ts
+++ src/datastructures/heaps/MaxBinaryHeap.ts
@@ -13,7 +13,24 @@ class MaxBinaryHeap<T> implements IMaxBinaryHeap<T> {
         this.swim();
     }
 
-    public swim(): void {
+    public extractMax(): T | null {
+        let rv: T | null;
+        if (this.items.length === 0) {
+            rv = null
+        } else if (this.items.length === 1) {
+            rv = this.items[0];
+            this.items.pop();
+        } else {
+            this.swap(0, this.items.length - 1);
+            rv = this.items[this.items.length - 1];
+            this.items.pop();
+            this.sink();
+        }
+
+        return rv;
+    }
+
+    private swim(): void {
         let index = this.items.length - 1;
         let parentIndex = Math.floor((index - 1) / 2);
 
@@ -24,6 +41,39 @@ class MaxBinaryHeap<T> implements IMaxBinaryHeap<T> {
             parentIndex = Math.floor((index - 1) / 2);
         }
     }
+
+    private sink(): void {
+        let index = 0;
+        const length = this.items.length;
+        const element = this.items[0];
+
+        while (true) {
+            let swap = null;
+            let leftChildIdx = index * 2 + 1;
+            let rightChildIdx = index * 2 + 2;
+
+            if (leftChildIdx < length) {
+                if (this.items[leftChildIdx] > this.items[index]) {
+                    swap = leftChildIdx;
+                }
+            }
+
+            if (rightChildIdx < length) {
+                if ((swap == null && this.items[rightChildIdx] > this.items[index]) ||
+                    (swap != null && this.items[rightChildIdx] > this.items[leftChildIdx])) {
+                    swap = rightChildIdx;
+                }
+            }
+
+            if (swap == null) {
+                break;
+            } else {
+                this.items[index] = this.items[swap];
+                this.items[swap] = element;
+                index = swap;
+            } // end if
+        } // end while
+    } // end fn sink
 
     private swap(idx1: number, idx2: number): void {
         let temp: T = this.items[idx1];
